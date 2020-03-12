e02dd2a647f631a62d5924f4c9b2a5dd4e9e8fea
3amer <mahmedabdallah@fci.helwan.edu.eg>
2020-03-12 18:29:15 +0200

    HTTPS SpringBoot JKS/P12 Certifications

---
+++ src/main/java/com/vodafone/portal/https/HttpsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.portal.https;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpsPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vodafone/portal/https/HttpsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.portal.https;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpsPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
